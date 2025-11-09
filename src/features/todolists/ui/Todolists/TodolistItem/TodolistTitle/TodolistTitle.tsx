502eec3532694cfc534d9dd564affc62e599f9fa
alexpodelinskii <nikitik007@yandex.ru>
2025-11-10 00:11:15 +0300

    refactor: add axios in project

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,39 +1,35 @@
-import {useAppDispatch} from '@/common/hooks/useAppDispatch'
-import {EditableSpan} from '@/common/components/EditableSpan/EditableSpan'
-import {
-  changeTodolistTitleAC,
-  deleteTodolistAC,
-  type Todolist
-} from '@/features/todolists/model/todolists-reducer'
-import DeleteIcon from '@mui/icons-material/Delete'
-import IconButton from '@mui/material/IconButton'
-import styles from './TodolistTitle.module.css'
+import { useAppDispatch } from '@/common/hooks';
+import { EditableSpan } from '@/common/components';
+import { changeTodolistTitleAC, deleteTodolistAC, type Todolist } from '@/features/todolists/model/todolists-reducer';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import styles from './TodolistTitle.module.css';
 
 type Props = {
-  todolist: Todolist
-}
+	todolist: Todolist;
+};
 
-export const TodolistTitle = ({todolist}: Props) => {
-  const {id, title} = todolist
+export const TodolistTitle = ({ todolist }: Props) => {
+	const { id, title } = todolist;
 
-  const dispatch = useAppDispatch()
+	const dispatch = useAppDispatch();
 
-  const deleteTodolist = () => {
-    dispatch(deleteTodolistAC({id}))
-  }
+	const deleteTodolist = () => {
+		dispatch(deleteTodolistAC({ id }));
+	};
 
-  const changeTodolistTitle = (title: string) => {
-    dispatch(changeTodolistTitleAC({id, title}))
-  }
+	const changeTodolistTitle = (title: string) => {
+		dispatch(changeTodolistTitleAC({ id, title }));
+	};
 
-  return (
-      <div className={styles.container}>
-        <h3>
-          <EditableSpan value={title} onChange={changeTodolistTitle}/>
-        </h3>
-        <IconButton onClick={deleteTodolist}>
-          <DeleteIcon/>
-        </IconButton>
-      </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<h3>
+				<EditableSpan value={title} onChange={changeTodolistTitle} />
+			</h3>
+			<IconButton onClick={deleteTodolist}>
+				<DeleteIcon />
+			</IconButton>
+		</div>
+	);
+};
